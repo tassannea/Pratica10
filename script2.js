76fece42f8cb0228b7337e13dd4a50d5a23358bd
Tássanne Ayshe <tassannea@gmail.com>
2025-09-30 11:41:45 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,20 @@
+//arrays -> vetores, matrizes
+//estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maçã", "Melancia"];//array de tamanho 3
+console.log(frutas);
+console.log(frutas[1]);//arrey começa do 0 - fruta 1 seria a maçã
+
+let numeros = new Array(2,8,9,10);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Gaby";
+console.log(arrayVazio);
+
+//Tamanho do array -> length 
+let dados = [10,50,30]; //tamanho 3 -> length 3
+console.log(dados.length);
+console.log(dados[1]);
+let ultimoElemento = numero
